c18f2333140da0766e9b1560ceeb4f359643e71c
pinyink <nurkhafindi@gmail.com>
2022-06-14 21:20:49 +0700

    php array

---
+++ php-array/initializing-array.php
@@ -0,0 +1,13 @@
+<?php
+
+$buah = array("anggur", "apel", "semangka");
+var_dump($buah);
+
+$sayur = ["kol", "timun", "tomat"];
+var_dump($sayur);
+
+$buah = array(
+    "first"=>"anggur",
+    "second" => "apel",
+    "third" => "semangka"
+);
